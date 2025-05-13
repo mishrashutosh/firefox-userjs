d29b784db0fef6cee0bd897040e8754c2bb10007
Ashutosh Mishra <a@ashutoshmishra.org>
2025-05-13 09:39:14 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,53 @@
+// these are generic privacy and usability enhancing configs that should be useful for everyone
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.first_run", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.newtabLayouts.variant-b", false);
+user_pref("browser.newtabpage.activity-stream.newtabWallpapers.wallpaper", "eclipse-time-lapse");
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.activity-stream.topSitesRows", 4);
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.usage.uploadEnabled", false);
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.ui.dictionary.hidden", false);
+user_pref("extensions.ui.sitepermission.hidden", false);
+user_pref("media.eme.enabled", true);
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("trailhead.firstrun.didSeeAboutWelcome", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("browser.promo.focus.enabled", false);
+user_pref("browser.promo.pin.enabled", false);
+user_pref("browser.promo.focus.enabled", false);
+user_pref("browser.vpn_promo.enabled", false);
+user_pref("dom.event.contextmenu.enabled", false);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("network.IDN_show_punycode", true);
+user_pref("plugins.enumerable_names", false);
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+
+
+// these configs are specific to my setup, may not be useful for everyone
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"vertical-spacer\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"logins-button\",\"downloads-button\",\"screenshot-button\",\"developer-button\",\"panic-button\",\"privatebrowsing-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"PersonalToolbar\",\"TabsToolbar\",\"toolbar-menubar\",\"unified-extensions-area\"],\"currentVersion\":22,\"newElementCount\":3}");
+user_pref("media.av1.enabled", false);
+user_pref("widget.gtk.non-native-titlebar-buttons.enabled", false);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("font.name.sans-serif.x-orya", "Noto Sans Oriya");
+user_pref("font.name.serif.x-orya", "Noto Sans Oriya");
+
+
+// this disables firefox sync. only use if you have no plans of using firefox sync
+user_pref("identity.fxaccounts.enabled", false);
